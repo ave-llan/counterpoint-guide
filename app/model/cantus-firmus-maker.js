72592f6b1bf95ee54627fa2d8b67fe4af8dd1fb0
John Leszczynski <jrleszczynski@gmail.com>
2015-09-28 17:52:41 -0400

    add choicePaths getter

--- app/model/cantus-firmus-maker.js
+++ app/model/cantus-firmus-maker.js
@@ -1,19 +1,31 @@
 var CantusFirmus = require('counterpoint').CantusFirmus
 var Key = require('nmusic').Key
 var sortPitches = require('nmusic').sortPitches
+var deepcopy = require('deepcopy')
 
-var CantusFirmusMaker = function (key, firstNote, maxRange, maxLength) {
-  key = key || 'C major'        // key of the cf
-  var keyParts = key.split(' ')
-  firstNote = firstNote || keyParts[0]
+/**
+ * helps to build a new cantus firmus
+ *
+ * @constructor
+ *
+ * @param {PitchString} [firstNote='C4'] - the tonic and first note
+ * @param {string} [mode='major'] - the mode (ie, 'major', 'minor', 'dorian') of this cf
+ * @param {number} [maxRange=10] - the max allowed range of this cf
+ * @param {number} [maxLength=16] - the max allowed length of this cf
+ */
+var CantusFirmusMaker = function (firstNote, mode, maxRange, maxLength) {
+  firstNote = firstNote || 'C4'
+  mode = mode || 'major'
   maxRange = maxRange || 10     // max range of cf
   maxLength = maxLength || 16   // max length of cf
+  var key = new Key(firstNote, mode)
 
-  var cf = new CantusFirmus(key, maxRange, maxLength)
+  var cf = new CantusFirmus(key.toString(), maxRange, maxLength)
+  // add the first note
   cf.addNote(firstNote)
 
   this.key = function () {
-    return key
+    return key.toString()
   }
 
   this.maxRange = function () {
@@ -53,16 +65,43 @@ var CantusFirmusMaker = function (key, firstNote, maxRange, maxLength) {
    * @returns {string[]}
    */
   this.domain = function () {
-    var keyParts = key.split(' ')
-    var cfKey = new Key(keyParts[0], keyParts[1])
     var usedNotes = sortPitches(this.construction())
     var lo = usedNotes[0]
     var hi = usedNotes[usedNotes.length - 1]
-    var pitches = cfKey.range(cfKey.plusInterval(hi, -1 * maxRange),
-                              cfKey.plusInterval(lo, maxRange))
+    var pitches = key.range(key.plusInterval(hi, -1 * maxRange),
+                              key.plusInterval(lo, maxRange))
     return pitches.map(function (pitch) {
       return pitch.sciPitch()
     })
+  }
+
+  /**
+   * returns an array of choices
+   *
+   */
+  this.choicePaths = function (nDeep) {
+    return cf.choices(nDeep).map(pathsFromNode).reduce(mergeArrays)
+  }
+}
+
+var mergeArrays = function (array1, array2) {
+  var merged = deepcopy(array1)
+  Array.prototype.push.apply(merged, array2)
+  return merged
+}
+
+var pathsFromNode = function (treeNode) {
+  if (treeNode.next.length === 0) {
+    return [[treeNode.val]]
+  } else {
+    var paths = []
+    treeNode.next.forEach(function (nextTreeNode) {
+      pathsFromNode(nextTreeNode).forEach(function (path) {
+        path.unshift(treeNode.val) // this val to front of path
+        paths.push(path)
+      })
+    })
+    return paths
   }
 }
 
