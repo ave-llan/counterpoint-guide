1b9c387ba1ae4fdb2731beb059ececd3c5acd422
John Leszczynski <jrleszczynski@gmail.com>
2015-09-29 14:51:13 -0400

    style: name xScale and yScale as just x and y

--- app/view/cantus-firmus-guide.js
+++ app/view/cantus-firmus-guide.js
@@ -23,29 +23,29 @@ var height = 500 - margin.top - margin.bottom
 var yAxisWidth = 44
 var nextChoiceDepth = 2
 
-var yScale = d3.scale.ordinal()
+var y = d3.scale.ordinal()
     .domain(cf.domain())
     .rangeRoundBands([height, 0], 0.05)
 
-var xScale = d3.scale.ordinal()
+var x = d3.scale.ordinal()
     // min 8, at least cur + 2
     .domain(d3.range(d3.max([8, cf.length() + 2])))
     .rangeRoundBands([yAxisWidth, width], 0.05)
 
 var constructionLine = d3.svg.line()
-    .x(function (d, i) { return xScale(i) })
-    .y(function (d) { return yScale(d) })
+    .x(function (d, i) { return x(i) })
+    .y(function (d) { return y(d) })
     .interpolate('monotone')
 
 var choicesLine = d3.svg.line()
-    .x(function (d, i) { return xScale(cf.length() - 1 + i) })
-    .y(function (d) { return yScale(d) })
+    .x(function (d, i) { return x(cf.length() - 1 + i) })
+    .y(function (d) { return y(d) })
     .interpolate('monotone')
 
 // for use in animation after choosing a path (what is the path between last two notes)
 var chosenPathLine = d3.svg.line()
-    .x(function (d, i) { return xScale(cf.length() - 2 + i)})
-    .y(function (d) { return yScale(d) })
+    .x(function (d, i) { return x(cf.length() - 2 + i)})
+    .y(function (d) { return y(d) })
     .interpolate('monotone')
 
 // Create SVG element
@@ -69,7 +69,7 @@ svg.append('g')
     .attr('text-anchor', 'start')
     .attr('dominant-baseline', 'central')
     .attr('x', 0)
-    .attr('y', function (d) { return yScale(d.val) })
+    .attr('y', function (d) { return y(d.val) })
 
 // add path of current cf construction
 svg.append('path')
@@ -83,8 +83,8 @@ svg.append('g')
   .selectAll('circle')
     .data(cf.construction())
   .enter().append('circle')
-    .attr('cx', function (d, i) { return xScale(i) })
-    .attr('cy', function (d) { return yScale(d) })
+    .attr('cx', function (d, i) { return x(i) })
+    .attr('cy', function (d) { return y(d) })
 
 var lastNote = cf.construction()[cf.length() - 1]
 var choicePaths = cf.choices().map(function (nextChoice) {
@@ -106,8 +106,8 @@ svg.append('g')
   .selectAll('circle')
     .data(cf.choices())
   .enter().append('circle')
-    .attr('cx', xScale(cf.length()))
-    .attr('cy', function (d) { return yScale(d) })
+    .attr('cx', x(cf.length()))
+    .attr('cy', function (d) { return y(d) })
     .on('click', function (d, i) {
       chosenPath = choicePaths.map(function () {
         return choicePaths[i]
@@ -118,11 +118,11 @@ svg.append('g')
 
 function redraw (svg) {
   // update scales
-  yScale = d3.scale.ordinal()
+  y = d3.scale.ordinal()
     .domain(cf.domain())
     .rangeRoundBands([height, 0], 0.05)
 
-  xScale = d3.scale.ordinal()
+  x = d3.scale.ordinal()
     .domain(d3.range(d3.max([8, cf.length() + 2])))
     .rangeRoundBands([yAxisWidth, width], 0.05)
 
@@ -142,7 +142,7 @@ function redraw (svg) {
       .duration(1000)
       .text(function (d) { return Pitch(d.val).pitchClass() })
       .attr('sciPitch', function (d) { return d.val })
-      .attr('y', function (d) { return yScale(d.val) })
+      .attr('y', function (d) { return y(d.val) })
 
   // recalcuate current construction y position to match new scale
   var constructionPoints = svg.select('.construction-points').selectAll('circle')
@@ -150,8 +150,8 @@ function redraw (svg) {
   // update old points
   constructionPoints.transition()
       .duration(1000)
-      .attr('cx', function (d, i) { return xScale(i) })
-      .attr('cy', function (d) { return yScale(d) })
+      .attr('cx', function (d, i) { return x(i) })
+      .attr('cy', function (d) { return y(d) })
   // update old construction line
   svg.select('#construction-line')
       // .datum(cf.construction())
@@ -164,8 +164,8 @@ function redraw (svg) {
   svg.select('.choice-points').selectAll('circle')
       .transition()
       .duration(1000)
-      .attr('cx', xScale(cf.length() - 1))
-      .attr('cy', yScale(pickedNote))
+      .attr('cx', x(cf.length() - 1))
+      .attr('cy', y(pickedNote))
       .remove()
 
   svg.select('.choice-paths').selectAll('path')
