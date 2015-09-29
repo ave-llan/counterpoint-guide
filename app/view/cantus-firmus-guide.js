54287906fb7dab8576ab42322adef0fbf35d6749
John Leszczynski <jrleszczynski@gmail.com>
2015-09-29 07:24:25 -0400

    style: use Mike Bostock's recommended indentation style

--- app/view/cantus-firmus-guide.js
+++ app/view/cantus-firmus-guide.js
@@ -2,96 +2,80 @@ var d3 = require('d3')
 var CFguide = require('../model/cantus-firmus-maker.js')
 var Pitch = require('nmusic').Pitch
 
-var cf = new CFguide('D4', 'major', 8)
-'B4'.split(' ').forEach(cf.addNote)
+var cf = new CFguide('D4', 'minor', 6)
+'E4 F4 C4 D4 F4 E4 G4 Bb3 C4 F4 E4 D4'.split(' ').forEach(cf.addNote)
 
 var margin = {top: 20, right: 10, bottom: 20, left: 10}
 var width = 600 - margin.left - margin.right
 var height = 500 - margin.top - margin.bottom
 var yAxisWidth = 44
-var nextChoiceDepth = 3
+var nextChoiceDepth = 2
 
 var yScale = d3.scale.ordinal()
-               .domain(cf.domain())
-               .rangeRoundBands([height, 0], 0.05)
+    .domain(cf.domain())
+    .rangeRoundBands([height, 0], 0.05)
+console.log(yScale('G4'))
 
 var xScale = d3.scale.ordinal()
-               .domain(d3.range(cf.maxLength()))
-               .rangeRoundBands([yAxisWidth, width], 0.05)
+    .domain(d3.range(cf.maxLength()))
+    .rangeRoundBands([yAxisWidth, width], 0.05)
 
 var constructionLine = d3.svg.line()
-    .x(function (d, i) {
-      return xScale(i)
-    })
-    .y(function (d) {
-      return yScale(d)
-    })
+    .x(function (d, i) { return xScale(i) })
+    .y(function (d) { return yScale(d) })
     .interpolate('monotone')
 
 var choicesLine = d3.svg.line()
-    .x(function (d, i) {
-      return xScale(cf.construction().length - 1 + i)
-    })
-    .y(function (d) {
-      return yScale(d)
-    })
+    .x(function (d, i) { return xScale(cf.construction().length - 1 + i) })
+    .y(function (d) { return yScale(d) })
     .interpolate('monotone')
 
 // Create SVG element
-var svg = d3.select('counterpoint').append('svg')
+var svg = d3.select('counterpoint')
+  .append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
   .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
 svg.selectAll('text')
-  .data(cf.domain())
-  .enter()
-  .append('text')
-  .text(function (d) {
-    return Pitch(d).pitchClass()
-  })
-  .attr('text-anchor', 'start')
-  .attr('dominant-baseline', 'central')
-  .attr('x', 0)
-  .attr('y', function (d) {
-    return yScale(d)
-  })
-  .attr('font-family', 'sans-serif')
-  .attr('font-size', '18px')
+    .data(cf.domain())
+  .enter().append('text')
+    .text(function (d) { return Pitch(d).pitchClass() })
+    .attr('text-anchor', 'start')
+    .attr('dominant-baseline', 'central')
+    .attr('x', 0)
+    .attr('y', function (d) { return yScale(d) })
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', '18px')
 
 // add path of current cf construction
 svg.append('path')
-      .datum(cf.construction())
-      .attr('class', 'construction-line')
-      .attr({
-        'fill': 'none',
-        'stroke': 'steelblue',
-        'stroke-width': '4px',
-        'stroke-linecap': 'round'
-      })
-      .attr('d', constructionLine)
+    .datum(cf.construction())
+    .attr('class', 'construction-line')
+    .attr({
+      'fill': 'none',
+      'stroke': 'steelblue',
+      'stroke-width': '4px',
+      'stroke-linecap': 'round'
+    })
+    .attr('d', constructionLine)
 
 // add points of current cf construction
 svg.append('g')
-  .attr('id', 'construction-points')
+    .attr('id', 'construction-points')
   .selectAll('circle')
-  .data(cf.construction())
-  .enter()
-  .append('circle')
-  .attr({
-    'fill': 'steelblue',
-    'fill-opacity': '0.9',
-    'stroke': 'steelblue',
-    'stroke-width': '1.5px'
-  })
-  .attr('cx', function (d, i) {
-    return xScale(i)
-  })
-  .attr('cy', function (d) {
-    return yScale(d)
-  })
-  .attr('r', 6)
+    .data(cf.construction())
+  .enter().append('circle')
+    .attr({
+      'fill': 'steelblue',
+      'fill-opacity': '0.9',
+      'stroke': 'steelblue',
+      'stroke-width': '1.5px'
+    })
+    .attr('cx', function (d, i) { return xScale(i) })
+    .attr('cy', function (d) { return yScale(d) })
+    .attr('r', 6)
 
 var lastNote = cf.construction()[cf.construction().length - 1]
 var choicePaths = cf.choicePaths(nextChoiceDepth).map(function (choicePath) {
@@ -99,38 +83,34 @@ var choicePaths = cf.choicePaths(nextChoiceDepth).map(function (choicePath) {
   return choicePath
 })
 
-// add paths of next note choices
+// add paths to next note choices
 svg.append('g')
-  .attr('id', 'choice-path')
+    .attr('id', 'choice-path')
   .selectAll('path')
-  .data(choicePaths)
-  .enter()
-  .append('path')
-  .attr({
-    'fill': 'none',
-    'stroke': 'steelblue',
-    'stroke-width': '1px',
-    'stroke-linecap': 'round',
-    'stroke-opacity': '0.1'
-  })
-  .attr('d', choicesLine)
+    .data(choicePaths)
+  .enter().append('path')
+    .attr({
+      'fill': 'none',
+      'stroke': 'steelblue',
+      'stroke-width': '1.5px',
+      'stroke-linecap': 'round',
+      'stroke-opacity': '0.3'
+    })
+    .attr('d', choicesLine)
 
 // add points of choices
 svg.append('g')
-  .attr('id', 'choices-points')
+    .attr('id', 'choices-points')
   .selectAll('circle')
-  .data(cf.choices())
-  .enter()
-  .append('circle')
-  .attr({
-    'fill': 'steelblue',
-    'fill-opacity': '0.3',
-    'stroke-opacity': '0.3',
-    'stroke': 'steelblue',
-    'stroke-width': '1.5px'
-  })
-  .attr('cx', xScale(cf.construction().length))
-  .attr('cy', function (d) {
-    return yScale(d)
-  })
-  .attr('r',6)
+    .data(cf.choices())
+  .enter().append('circle')
+    .attr({
+      'fill': 'steelblue',
+      'fill-opacity': '0.3',
+      'stroke-opacity': '0.3',
+      'stroke': 'steelblue',
+      'stroke-width': '1.5px'
+    })
+    .attr('cx', xScale(cf.construction().length))
+    .attr('cy', function (d) { return yScale(d) })
+    .attr('r',6)
