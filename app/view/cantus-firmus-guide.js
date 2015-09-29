d3500b1e8d277e25a0e51db1a00b851def9ebef9
John Leszczynski <jrleszczynski@gmail.com>
2015-09-29 10:01:03 -0400

    move general styles to stylesheet

--- app/view/cantus-firmus-guide.js
+++ app/view/cantus-firmus-guide.js
@@ -50,45 +50,33 @@ var svg = d3.select('counterpoint')
   .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-svg.selectAll('text')
+// add note names to y axis
+svg.append('g')
+    .attr('class', 'y-axis-text')
+  .selectAll('text')
     .data(cf.domain())
   .enter().append('text')
     .text(function (d) { return Pitch(d).pitchClass() })
+    .attr('sciPitch', function (d) { return d })
     .attr('text-anchor', 'start')
     .attr('dominant-baseline', 'central')
     .attr('x', 0)
     .attr('y', function (d) { return yScale(d) })
-    .attr('font-family', 'sans-serif')
-    .attr('font-size', '18px')
 
 // add path of current cf construction
 svg.append('path')
     .datum(cf.construction())
-    .attr('class', 'construction-line')
-    .attr({
-      'fill': 'none',
-      'stroke': 'steelblue',
-      'stroke-width': '16px',
-      'stroke-linecap': 'round',
-      'stroke-opacity': '0.5'
-    })
+    .attr('id', 'construction-line')
     .attr('d', constructionLine)
 
 // add points of current cf construction
 svg.append('g')
-    .attr('id', 'construction-points')
+    .attr('class', 'construction-points')
   .selectAll('circle')
     .data(cf.construction())
   .enter().append('circle')
-    .attr({
-      'fill': 'steelblue',
-      'fill-opacity': '1',
-      'stroke': 'steelblue',
-      'stroke-width': '0px'
-    })
     .attr('cx', function (d, i) { return xScale(i) })
     .attr('cy', function (d) { return yScale(d) })
-    .attr('r', 8)
 
 var lastNote = cf.construction()[cf.construction().length - 1]
 var choicePaths = cf.choices().map(function (nextChoice) {
@@ -97,32 +85,17 @@ var choicePaths = cf.choices().map(function (nextChoice) {
 
 // add paths to next note choices
 svg.append('g')
-    .attr('id', 'choice-path')
+    .attr('class', 'choice-paths')
   .selectAll('path')
     .data(choicePaths)
   .enter().append('path')
-    .attr({
-      'fill': 'none',
-      'stroke': 'steelblue',
-      'stroke-width': '1.5px',
-      'stroke-linecap': 'round',
-      'stroke-opacity': '0.3'
-    })
     .attr('d', choicesLine)
 
 // add points of choices
 svg.append('g')
-    .attr('id', 'choices-points')
+    .attr('class', 'choice-points')
   .selectAll('circle')
     .data(cf.choices())
   .enter().append('circle')
-    .attr({
-      'fill': 'steelblue',
-      'fill-opacity': '0.3',
-      'stroke-opacity': '0.3',
-      'stroke': 'steelblue',
-      'stroke-width': '1.5px'
-    })
     .attr('cx', xScale(cf.construction().length))
     .attr('cy', function (d) { return yScale(d) })
-    .attr('r',6)
