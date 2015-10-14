384f401d85d05a7ce33df7d84f1d4c3c4a58ea12
John Leszczynski <jrleszczynski@gmail.com>
2015-10-13 20:19:28 -0400

    add tests for transposing

--- test/model/cantus-firmus-maker.test.js
+++ test/model/cantus-firmus-maker.test.js
@@ -13,6 +13,32 @@ test('CantusFirmusMaker', function (t) {
   t.deepEqual(cf.domain(),
     [ 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5' ])
 
+  t.deepEqual(cf.construction(),
+    [ 'C4', 'C5' ])
+
+  var cf_transposed_up = cf.transpose('P5')
+  var cf_transposed_down = cf.transpose('-M2')
+
+
+  t.deepEqual(cf.construction(),
+    [ 'C4', 'C5' ])
+
+  t.deepEqual(cf_transposed_up.construction(),
+    [ 'G4', 'G5' ])
+
+  t.deepEqual(cf_transposed_down.construction(),
+    [ 'Bb3', 'Bb4' ])
+
+  cf_transposed_up.addNote('F#5')
+
+  t.deepEqual(cf.construction(),
+    [ 'C4', 'C5' ])
+
+  t.deepEqual(cf_transposed_up.construction(),
+    [ 'G4', 'G5', 'F#5' ])
+
+  t.deepEqual(cf_transposed_down.construction(),
+    [ 'Bb3', 'Bb4' ])
 
   t.end()
 })
